d235b96e41c29836711a43f5475044bc181ea6ca
Ryan McCullough <mccullough.ryan123@gmail.com>
2020-12-12 19:26:34 -0500

    small update

--- include/Serenity/Helpers.h
+++ include/Serenity/Helpers.h
@@ -36,7 +36,8 @@ namespace helper_color {
 
 	inline FILE* GetStdStream(const std::ostream& stream);
 	inline bool isColored(std::ostream& stream);
-	inline bool is_atty(const std::ostream& stream);
+	// Basically isatty posix function warpper for validating output stream from the <io.h> header
+	inline bool isCharDevice(const std::ostream& stream);
 
 #if defined(SERENITY_WINDOWS)
 	//? background = -1?
