fcd55eb8c5a20c2865f6819948ee245aba0950c7
Ryan McCullough <mccullough.ryan123@gmail.com>
2020-12-22 21:34:23 -0500

    Added Default Example From Catch2

--- Test/src/TestMain.cpp
+++ Test/src/TestMain.cpp
@@ -1,12 +1,25 @@
+#define CATCH_CONFIG_MAIN
+
 #include <catch2/catch.hpp>
 
-#include <iostream>
 
-int main( )
+// Factorial Test Ripped From Catch2's Examples - Just Wanted To Check All Is Linked Up Well
+
+static int Factorial(int number)
 {
-	std::cout << "Test main says hi! =D\n";
+	return number <= 1 ? number : Factorial(number - 1) * number; // fail
+	// return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
+}
 
-	char garbage = std::getchar( );
+TEST_CASE("Factorial of 0 is 1 (fail)", "[single-file]")
+{
+	REQUIRE(Factorial(0) == 1);
+}
 
-	return 0;
+TEST_CASE("Factorials of 1 and higher are computed (pass)", "[single-file]")
+{
+	REQUIRE(Factorial(1) == 1);
+	REQUIRE(Factorial(2) == 2);
+	REQUIRE(Factorial(3) == 6);
+	REQUIRE(Factorial(10) == 3628800);
 }
